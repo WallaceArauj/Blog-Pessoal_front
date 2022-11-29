2ef95d5b34bb542331ab71475fd548e5a4042cb1
WallaceArauj <wallace.hostile94@gmail.com>
2022-11-29 16:38:41 -0300

    feat: added hook state

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import './Home.css';
 
 function Home () {
+    const [num, setNum] = useState(0); // esta váriavel ele retorna um vetor com duas informações, que trás todas as informações do vetor e toda função
+    
     return (
-        <h1>Página Home</h1>
+        <>
+            <h1>Vc clicou {num} vezes</h1>
+            <button onClick={()=> setNum(num + 1)}>
+            clique aqui
+            </button>
+            <button onClick={()=> setNum(num - 1)}>
+            clique aqui
+            </button>
+        </>
     );
 }
 
